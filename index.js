e074e8b037489d2c8e07ba21f5cb161463508cab
mdss-dev <douglas.ti.dev@gmail.com>
2022-01-20 18:52:15 -0300

    install cors e winston

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import cors from "cors";
+import winston from "winston";
+
 import clientsRouter from "./routes/client.route.js";
 import productsRouter from "./routes/product.route.js";
 import salesRouter from "./routes/sale.route.js";
@@ -7,6 +10,7 @@ import suppliersRouter from "./routes/supplier.route.js";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/client", clientsRouter);
 app.use("/product", productsRouter);
