50f5c7dde0764120ef4f432b5fca77326dfc0f4f
mdss-dev <douglas.ti.dev@gmail.com>
2022-01-20 19:05:07 -0300

    winston configurado ('logs')

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ import productsRouter from "./routes/product.route.js";
 import salesRouter from "./routes/sale.route.js";
 import suppliersRouter from "./routes/supplier.route.js";
 
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level} ${message}`;
+});
+global.logger = winston.createLogger({
+  level: "silly",
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "store-api.log" }),
+  ],
+  format: combine(label({ label: "store-api" }), timestamp(), myFormat),
+});
+
 const app = express();
 
 app.use(express.json());
