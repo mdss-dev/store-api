532bfac97c8e20faed79f02cc3c0273fd7a32748
mdss-dev <douglas.ti.dev@gmail.com>
2022-01-20 18:45:46 -0300

    rotas criadas

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import clientsRouter from "./routes/client.route.js";
+import productsRouter from "./routes/product.route.js";
+import salesRouter from "./routes/sale.route.js";
+import suppliersRouter from "./routes/supplier.route.js";
 
 const app = express();
+
+app.use("/client", clientsRouter);
+app.use("/product", productsRouter);
+app.use("/sale", salesRouter);
+app.use("/supllier", suppliersRouter);
 
 app.listen(3000, () => console.log("Api started!"));
