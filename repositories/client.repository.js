8a2e7cafc73eaa3d2db3ae16101051f580569a25
mdss-dev <douglas.ti.dev@gmail.com>
2022-01-23 15:56:00 -0300

    metodo para criar client proto e arquivo db.js movido para repositories

--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -1,4 +1,26 @@
-async function insertClient(client) {}
+import { connect } from "./db.js";
+
+//método para criar cliente
+async function insertClient(client) {
+  const conn = await connect();
+  try {
+    const sql =
+      "INSERT INTO clients (name, cpf, email, phone, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+    const values = [
+      client.name,
+      client.cpf,
+      client.email,
+      client.phone,
+      client.address,
+    ];
+    const res = await conn.query(sql, values);
+    return res.rows[0];
+  } catch (err) {
+    throw err;
+  } finally {
+    conn.release(); //fecha a conexão
+  }
+}
 
 export default {
   insertClient,
